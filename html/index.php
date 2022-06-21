9b1549d91483ba7b69580151085267cd677d5d84
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-21 13:46:05 -0400

    desktop mode for mobile

--- html/index.php
+++ html/index.php
@@ -7,20 +7,6 @@
   <meta name="viewport" content="width=device-width">
   <title>Guessit</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
-  <?php
-  $servername = "database_server";
-  $username = "root";
-  $password = "password";
-  
-  // Create connection
-  $conn = new mysqli($servername, $username, $password);
-  
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  echo "Connected successfully";
-  ?> 
 </head>
 
 <body>
--- html/script.js
+++ html/script.js
@@ -191,3 +191,9 @@ function updateLists() {
 function home() {
   window.location.href = "./index.php";
 }
+var viewMode = getCookie("view-mode");
+if (viewMode == "desktop") {
+    viewport.setAttribute('content', 'width=1024');
+} else if (viewMode == "mobile") {
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+}
