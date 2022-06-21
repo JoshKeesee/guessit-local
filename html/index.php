0143e7343aae451ec290336f5a105fb82d70c3c0
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-21 13:56:06 -0400

    rwd

--- html/index.php
+++ html/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <link rel='icon' type='image/xicon' href='icon.png'>
-  <meta name="viewport" content="width=device-width">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Guessit</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
