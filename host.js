87688ff4d9af2314a762e4f009c74a2b90dc4b30
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-27 14:46:39 -0400

    .

--- host.js
+++ host.js
@@ -1,3 +1,10 @@
+if (localStorage.getItem("codes") != "") {
+  var codes = JSON.parse(localStorage.getItem("codes"));
+  var gamecode = JSON.parse(codes[codes.length - 1]) + 1;
+} else {
+  var codes = [""];
+  var gamecode = JSON.parse(codes[codes.length - 1]) + 1;
+}
 var elem = document.documentElement;
 function openFullscreen() {
   if (elem.requestFullscreen) {
@@ -9,18 +16,13 @@ function openFullscreen() {
   }
 }
 function addgame() {
-  if (localStorage.getItem("codes") != "") {
-    var codes = JSON.parse(localStorage.getItem("codes"));
-  } else {
-    var codes = ["1"];
-  }
-  var gamecode = JSON.parse(codes[codes.length - 1]) + 1;
   codes.push(JSON.stringify(gamecode));
   localStorage.setItem("codes", JSON.stringify(codes));
   document.querySelector(".code").innerHTML = gamecode;
 }
 function removegame() {
   codes = localStorage.getItem("codes");
+  gamecode = localStorage.getItem("gamecode");
   codes.splice(gamecode - 1, 1);
   localStorage.setItem("codes", codes);
 }
