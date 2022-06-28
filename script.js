f9ec8c755b065da6a4b33ad0eadef2ef1006b624
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-28 10:28:30 -0400

    .

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ function scrollFunction() {
   }
 }
 function host() {
-  localStorage.setItem("players", [""]);
+  localStorage.setItem("players", JSON.stringify([""]));
   localStorage.setItem("start", "0");
   window.location.href = 'host';
 }
