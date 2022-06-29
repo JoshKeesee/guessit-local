200726c73f5a6c7bc4128432e56939a14ed5fff5
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-29 08:54:29 -0400

    .

--- host/play.js
+++ host/play.js
@@ -3,17 +3,18 @@ var timings = gup("time");
 var i = 0;  
 var myInterval = setInterval(Timeout, 1000);  
 function Timeout() {  
-    if ((timings * 60 - i) % 60 >= 10) {  
-        time[0].innerHTML = parseInt(`${(timings * 60 - i) / 60}`) + ":" + `${(timings * 60 - i) % 60}`;  
-    } else {  
-        time[0].innerHTML = parseInt(`${(timings * 60 - i) / 60}`) + ":0" + `${(timings * 60 - i) % 60}`;  
-    }  
-    i++;
+    if (document.querySelector("timer") != "0:00") {
+        if ((timings * 60 - i) % 60 >= 10) {  
+            time[0].innerHTML = parseInt(`${(timings * 60 - i) / 60}`) + ":" + `${(timings * 60 - i) % 60}`;  
+        } else {  
+            time[0].innerHTML = parseInt(`${(timings * 60 - i) / 60}`) + ":0" + `${(timings * 60 - i) % 60}`;  
+        }  
+        i++;
+    }
 }
 function checkIfDone() {
     if (document.querySelector(".timer").innerHTML === "0:00") {
         setTimeout(end, 5000);
-        clearInterval(Timeout);
     }
     setTimeout(checkIfDone, 0);
 }
--- lobby.js
+++ lobby.js
@@ -1,15 +1,16 @@
-function reload() {
+/*function reload() {
     setTimeout(reloadpage, 2000);
 }
 function reloadpage() {
     location.reload();
-}
+}*/
 function check() {
     if (localStorage.getItem("start") === "1") {
         var nickname = gup("nickname");
         var time = localStorage.getItem("time");
         window.location.href = "game?nickname=" + nickname + "?time=" + time;
     }
+    setTimeout(check, 0);
 }
 function gup (name) {
     name = RegExp ('[?&]' + name.replace (/([[\]])/, '\\$1') + '=([^&#]*)');
