8d0527a8409d4027f524dbd77a3814cccf216c2a
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-29 10:30:07 -0400

    .

--- host/final.js
+++ host/final.js
@@ -8,8 +8,20 @@ function showLeaderboard() {
         div.className = "leaderboard";
         div.innerHTML = "<span class='circle'>" + (i + 1) + "</span>" + data[i].name + " " + data[i].score;
         document.querySelector(".results").appendChild(div);
+        if (i === 0) {
+            document.querySelector(".circle").style.background = "gold";
+            var timeout = 1000;
+        } else if (i === 1) {
+            document.querySelectorAll(".circle")[1].style.background = "silver";
+            var timeout = 1000;
+        } else if (i === 2) {
+            document.querySelectorAll(".circle")[2].style.background = "#cd7f32";
+            var timeout = 1000;
+        } else {
+            var timeout = 200;
+        }
         i++;
-        setTimeout(showLeaderboard, 500);
+        setTimeout(showLeaderboard, timeout);
     }
 }
 function getScores() {
@@ -22,6 +34,6 @@ function getScores() {
         data[x].name = players[x];
         data[x].score = scores[x];
     }
-    data.sort((a,b) => a.score < b.score);
-    setTimeout(showLeaderboard, 0);
+    data = data.sort(function(a,b) { return b.score - a.score });
+    setTimeout(showLeaderboard, 1000);
 }
