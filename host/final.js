55cb1cd88686d454eee5396192943cd410a46db6
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-28 20:31:36 -0400

    .

--- host/final.js
+++ host/final.js
@@ -1,12 +1,12 @@
 var i = 0;
+var data = [];
 var scores = [];
 var players = JSON.parse(localStorage.getItem("players"));
 function showLeaderboard() {
-    var players = JSON.parse(localStorage.getItem("players"));
     if (i < players.length) {
         var div = document.createElement("div");
         div.className = "leaderboard";
-        div.innerHTML = "<span class='circle'>" + (i + 1) + "</span>" + players[i] + " " + scores[i];
+        div.innerHTML = "<span class='circle'>" + (i + 1) + "</span>" + data[i].name + " " + data[i].score;
         document.querySelector(".results").appendChild(div);
         i++;
         setTimeout(showLeaderboard, 500);
@@ -17,5 +17,11 @@ function getScores() {
         scores.push(localStorage.getItem(players[i]));
     }
     i = 0;
+    for (let x = 0; x < players.length; x++) {
+        data.push({name: "", score: ""});
+        data[x].name = players[x];
+        data[x].score = scores[x];
+    }
+    data.sort((a,b) => a.score < b.score);
     setTimeout(showLeaderboard, 0);
 }
