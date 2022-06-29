4a49bf2788eba540b59f19337e485c3cb4f7bfcd
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-29 09:04:45 -0400

    .

--- host/play.js
+++ host/play.js
@@ -3,7 +3,7 @@ var timings = gup("time");
 var i = 0;  
 var myInterval = setInterval(Timeout, 1000);  
 function Timeout() {  
-    if (document.querySelector(".timer") != "0:00") {
+    if (document.querySelector(".timer").innerHTML != "0:00") {
         if ((timings * 60 - i) % 60 >= 10) {  
             time[0].innerHTML = parseInt(`${(timings * 60 - i) / 60}`) + ":" + `${(timings * 60 - i) % 60}`;  
         } else {  
@@ -13,7 +13,7 @@ function Timeout() {
     }
 }
 function checkIfDone() {
-    if (document.querySelector(".timer").innerHTML != "0:00") {
+    if (document.querySelector(".timer").innerHTML === "0:00") {
         setTimeout(end, 5000);
     } else {
         setTimeout(checkIfDone, 0);
