43c1a5ed1beb8a99f192cf9b9f8bf234160ca2f1
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-30 02:43:04 -0400

    .

--- host/play.js
+++ host/play.js
@@ -47,7 +47,7 @@ function refreshData() {
     }, 1000);
 }
 function showLeaderboard() {
-    for (i = 0; i < document.querySelectorAll(".player").length; i++) {
+    for (i = 0; i < document.querySelectorAll(".player").length - 1; i++) {
         document.querySelectorAll(".player")[i].remove();
     }
     for (i = 0; i < data.length; i++) {
