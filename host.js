cce0bbe075c91938aba9001cf54c75dabbf6a142
Josh Keesee <joshuakeesee1@gmail.com>
2022-06-30 01:47:44 -0400

    .

--- host.js
+++ host.js
@@ -1,45 +1,42 @@
-reload();
-if (localStorage.getItem("codes") != "") {
-  var codes = JSON.parse(localStorage.getItem("codes"));
-  var gamecode = JSON.parse(codes[codes.length - 1]) + 1;
-} else {
-  var codes = ["0"];
-  var gamecode = JSON.parse(codes[codes.length - 1]) + 1;
-}
-if (JSON.parse(localStorage.getItem("players")) === null || JSON.parse(localStorage.getItem("players")) === "") {
-  var players = [""];
-} else {
-  var players = JSON.parse(localStorage.getItem("players"));
-}
+let prevPlayers = [];
+let gamecode;
+let players = [];
 var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) {
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { 
-    elem.msRequestFullscreen();
-  }
-}
 function addgame() {
+  if (localStorage.getItem("codes") != "") {
+    var codes = JSON.parse(localStorage.getItem("codes"));
+  } else {
+    var codes = ["1"];
+  }
+  if (JSON.parse(localStorage.getItem("players")) === null || JSON.parse(localStorage.getItem("players")) === "") {
+    players = [""];
+  } else {
+    players = JSON.parse(localStorage.getItem("players"));
+  }
+  gamecode = codes[codes.length - 1] + 1;
+  prevPlayers = players;
   codes.push(JSON.stringify(gamecode));
   localStorage.setItem("codes", JSON.stringify(codes));
   document.querySelector(".code").innerHTML = gamecode;
   localStorage.setItem("players", JSON.stringify(players));
+  refreshData();
 }
 function removegame() {
   codes = JSON.parse(localStorage.getItem("codes"));
   codes.splice(gamecode - 1, 1);
   localStorage.setItem("codes", JSON.stringify(codes));
 }
-function reload() {
-  setTimeout(reloadpage, 5000);
-}
-function reloadpage(r) {
-  location.reload();
+function refreshData() {
+  setInterval(function() { 
+    players = JSON.parse(localStorage.getItem("players")); 
+    if (prevPlayers != players) {
+      removePlayers();
+      addplayers();
+      prevPlayers = players;
+    }
+  }, 0)
 }
 function addplayers() {
-  players = JSON.parse(localStorage.getItem("players"));
   if (players[0] != "") {
     document.querySelector("#numplayers").innerHTML = players.length;
     for (let i = 0; i < players.length; i++) {
@@ -48,6 +45,11 @@ function addplayers() {
       div.innerHTML = players[i];
       document.body.appendChild(div);
     }
+  }
+}
+function removePlayers() {
+  for (let i = 0; i < document.querySelectorAll(".player").length; i++) {
+    document.querySelectorAll(".player")[i].remove();
   }
 }
 function start() {
